c4996d0ba7fbc6f84fb41eccd8818d240cc79c02
kavya8480 <tapaswi84802@gmail.com>
2026-01-27 20:41:43 +0530

    read me

--- string_num.js
+++ string_num.js
@@ -1,6 +1,27 @@
  str = 'hello';
  num=5;
  res=str+num;
-console.log(res);
-console.log(str);
+// console.log(res);
+// console.log(str);
 console.log("hello from js file.");
+
+console.error("alert", "hello", "jbghgnk");
+
+
+// let arr=["abc","xyz","def","ghi"];
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+
+// let arrnum=[10,20,30,40,50];
+// let arr1=arrnum.map((value)=>{
+//     return value+5;
+// });
+// console.log('arr is: ',arrnum);
+// console.log('arr is- ',arr1);
+
+// let arrfilter=[5,8,9,10,52,83,2,1];
+// let reslt = arrfilter.filter((value)=>{
+//     return value<5;
+// });
+// console.log('result is: ',reslt);
